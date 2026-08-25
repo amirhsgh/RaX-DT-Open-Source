743a227b869db1f2796f931ac8b27f357a07874f
amirhsgh <amiqavi2601@gmail.com>
2026-08-25 09:40:44 +0330

    Remove more dead frontend files (unused landing-page sections, NGL debug scaffolding)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,10 +8,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// Debug NGL
-console.log('Starting NGL debug...');
-import('./debug-ngl.js');
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
